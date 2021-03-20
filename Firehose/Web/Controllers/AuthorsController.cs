74ea6d986ff66846f73a16252f0df4893845528d
Saamer Mansoor <i@saamer.me>
2021-03-20 03:06:59 -0400

    Authors Page completed but without data

---
+++ Firehose/Web/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var viewModel = _members;
+            return View(viewModel);
+        }
+
+        private readonly IAmACommunityMember[] _members;
+
+        public AuthorsController(IEnumerable<IAmACommunityMember> members)
+        {
+            var random = new Random();
+            _members = members.OrderBy(r => random.Next()).ToArray();
+        }
+    }
+}
